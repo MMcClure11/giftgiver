20e15c8461be31886def36ccfff80ffd19ac1b4f
MMcClure11 <mcclme02@luther.edu>
2021-01-12 11:36:57 -0600

    Add newGift to state

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,23 @@ export default class App extends Component {
     this.state = { gifts: [] };
   }
 
+  addGift = () => {
+    const { gifts } = this.state;
+
+    const ids = this.state.gifts.map(gift => gift.id);
+
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({ id: max_id+1 });
+
+    this.setState({ gifts });
+  }
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
-        <Button className='btn-add'>Add Gift</Button>
+        <Button className='btn-add' onClick={ this.addGift }>Add Gift</Button>
       </div>
     )
   }
