632eeb843e01baff61f6adb315c92e6e229a1610
MMcClure11 <mcclme02@luther.edu>
2021-01-11 19:24:24 -0600

    Add configuration for test file

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 const app = shallow(<App />);
 
