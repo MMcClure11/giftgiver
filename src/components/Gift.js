efa2c6e40de0c08ec320ecf46fca4dcbb90712ea
MMcClure11 <mcclme02@luther.edu>
2021-01-14 13:44:11 -0600

    Add state to pass gift test

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Gift extends Component {
+
+  state = {
+    person: '',
+    present: '',
+  }
+
   render() {
     return (
       <div>
