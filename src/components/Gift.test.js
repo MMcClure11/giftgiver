d0ef439afeea2c1d9ae9e570818caf0b915e3177
MMcClure11 <mcclme02@luther.edu>
2021-01-14 13:40:22 -0600

    Add Gift class component, add necessary imports to run Gift.test suite

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import Gift from './Gift';
 
-// import Enzyme, { shallow } from 'enzyme';
-// import EnzymeAdapter from 'enzyme-adapter-react-16'
-// Enzyme.configure({ adapter: new EnzymeAdapter() })
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 
 describe('Gift', () => {
   const gift = shallow(<Gift />);
+
+  it('renders properly', () => {
+    expect(gift).toMatchSnapshot();
+  })
 });
